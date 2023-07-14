7ab395e6d7b40289277aa3cae7fe39916309ed50
Administrator <Administrator@304-23>
2023-07-14 17:14:41 +0900

    0714. aws config-server 연결/ list 페이징 정비, css 수정필요

--- src/test/java/com/green/nowon/SpringWebtoonApplicationTests.java
+++ src/test/java/com/green/nowon/SpringWebtoonApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.green.nowon.domain.entity.Category;
 import com.green.nowon.domain.entity.SeriesEntity;
 import com.green.nowon.domain.entity.SeriesImgEntity;
 import com.green.nowon.domain.entity.UserEntity;
@@ -46,5 +47,28 @@ class SpringWebtoonApplicationTests {
 		System.out.println(imgRepo.findBySeries(srepo.findById(3L).orElseThrow())); ;
 		
 	}
-
+	
+	@Test
+	void seriesCreate() {
+		
+		for (int i=1; i<=100; i++) {
+			SeriesEntity entity= SeriesEntity.builder()
+			.title("테스트작품 로맨스"+i)
+			.synopsis("이것은 로맨스 장르의 테스트 입력입니다. ")
+			.writer("테스트작가"+i)
+			.category(Category.ROMANCE)
+			.build();
+			//
+			
+			imgRepo.save(SeriesImgEntity.builder()
+					.url("../images/Cat.jpg")
+					.orgName("Cat.jpg")
+					.newName("Cat-"+i+".jpg")
+					.bucketKey("")
+					.series(srepo.save(entity))
+					.build());
+			
+		}
+		
+	}
 }
